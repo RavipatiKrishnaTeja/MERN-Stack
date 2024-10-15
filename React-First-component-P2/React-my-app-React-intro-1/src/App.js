b77b8505a269c91166e71b8f002e171132917456
Krishna Teja <ravipatikrishnateja5@gmail.com>
2024-10-15 10:47:46 +0530

    React-my-app-First-Component-Work

---
+++ React-First-component-P2/React-my-app-React-intro-1/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { FirstComponent } from "./FirstComponent";
+
+function App() {
+  return (
+    <>
+      <h1>this app</h1>
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+    </>
+  );
+}
+
+export default App;
+//fragment <></>
+
+//activity
+//try creating a Button component and import inside App.
---
+++ React-First-component-P2/React-my-app-React-intro-1/src/FirstComponent.js
@@ -0,0 +1,7 @@
+export function FirstComponent() {
+  return (
+    <div>
+      <h2>this is h2 tag</h2>
+    </div>
+  );
+}
